749a68b6dbdd297a11af33207fb0e218290c2851
dushibaiyu <dushibaiyu@yahoo.com>
2017-08-31 17:46:55 +0800

    data

--- src/main/kotlin/com/yu/spark/main.kt
+++ src/main/kotlin/com/yu/spark/main.kt
@@ -20,7 +20,7 @@ fun main(args:Array<String>)
     val start = LocalDate.now().atTime(0,0).toEpochSecond(ZoneOffset.UTC); // 当天开始的UNix时间戳
 
     val sql  = SparkSession.Builder().config(conf).getOrCreate().sqlContext();
-    var df  = sql.read().format("org.elasticsearch.spark.sql").load("ptlog_paios_power/85bfac7251c93e16b7b946ea5eded05d"); // 从es获取数据，spark sql接口
+    var df  = sql.read().format("org.elasticsearch.spark.sql").load("test_data/power"); // 从es获取数据，spark sql接口
     var playlist   = df.filter(df.col("access").equalTo("wifi").and(df.col("create_time").lt(nowTime)).and(df.col("create_time").gt(start))); // 过滤条件
 
     playlist.show(5); // 显示获取的前5条数据， 这儿才执行spark job的，上面过滤一类的都是懒加载的
@@ -35,3 +35,76 @@ fun main(args:Array<String>)
 
     println("tv = $packageName");
 }
+/*
+The json data
+ {
+    "took":6,
+    "timed_out":false,
+    "_shards":{
+        "total":5,
+        "successful":5,
+        "failed":0
+    },
+    "hits":{
+        "total":14048,
+        "max_score":null,
+        "hits":[
+            {
+                "_index":"test_data",
+                "_type":"power",
+                "_id":"AV43l57dgCn29LxtR0LH",
+                "_score":null,
+                "_source":{
+                    "access":"wifi",
+                    "access_subtype":"wifi",
+                    "app_id":"85bfac7251c93e16b7b946ea5eded05d",
+                    "app_version":"PD00C00A02B071",
+                    "carrier":"",
+                    "channel":"PaiOS",
+                    "country":"CN",
+                    "cpu":"arm64-v8a",
+                    "create_time":1504171237,
+                    "device_board":"Android",
+                    "device_brand":"Android",
+                    "device_id":"test123",
+                    "device_manufacturer":"Android",
+                    "device_model":"test",
+                    "device_name":"test",
+                    "ip":"0.0.0.0",
+                    "language":"zh",
+                    "launch_time":"1504172304",
+                    "log_type":"test-pw",
+                    "mc":"00:00:00:00:00:00",
+                    "open_data":{
+                        "alarmWakeups":0,
+                        "appVersion":"1.0.7",
+                        "bluetoothUsage":0,
+                        "cameraUsage":0,
+                        "cpuPowerUsage":0,
+                        "memoryUsage":12,
+                        "networkUsage":0,
+                        "packageName":"com.test.aaa",
+                        "processCount":1,
+                        "timeSlot":1776461,
+                        "timestamp":1504172304166,
+                        "type":"power",
+                        "wakelockCount":0,
+                        "wakelockTime":0
+                    },
+                    "os":"Android",
+                    "os_version":"HHH 1.4.0",
+                    "package_name":"HH",
+                    "resolution":"2048*1536",
+                    "sdk_type":"Android",
+                    "sdk_version":"1.1.1",
+                    "timezone":"8",
+                    "user_id":""
+                },
+                "sort":[
+                    1504171237
+                ]
+            }
+        ]
+    }
+}
+* */
